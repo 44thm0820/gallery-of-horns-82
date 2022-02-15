207c5e82348abf598b7a9b4db27447040a16efd7
Rey Mercado <reymercado.usa@gmail.com>
2022-02-15 15:45:21 -0800

    completed first of two tasks in feature2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  // THIS is how we set up state in a component (reference 1:49 in Day2 video)
+  constructor(props) {
+    super(props);
+    this.state ={
+      likes: 0,
+    }
+  }
+
+  handleLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1,
+    })
+  }
+
   render() {
     return (
       <article className="card">
         <header>
           <h2>{this.props.title}</h2>
         </header>
-        <img 
+        <img onClick={this.handleLikes}
           src={this.props.imageUrl} 
           alt={this.props.description}
           title={this.props.title} 
         />
+        <p>👍: {this.state.likes}</p>
         <p>{this.props.description}</p>
       </article>
     );
